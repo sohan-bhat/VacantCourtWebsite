15720e018da52a3aed62c5a01c7685913cd0dadb
sohan-bhat <sohanrambhat@gmail.com>
2025-05-26 13:22:04 -0500

    Added new icon for no courts available leaflet

--- src/components/CourtMap.tsx
+++ src/components/CourtMap.tsx
@@ -20,6 +20,29 @@ interface CourtMapProps {
     isProximityFilteringActive: boolean;
 }
 
+const createStrikethroughIcon = (isUnavailable: boolean) => {
+    const defaultIconUrl = 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png';
+    const defaultIconRetinaUrl = 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png';
+    const defaultShadowUrl = 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png';
+    const iconSize: [number, number] = [25, 41];
+
+    const htmlContent = `
+        <div class="leaflet-marker-icon-wrapper ${isUnavailable ? 'marker-unavailable-strikethrough' : ''}">
+            <img src="${defaultIconUrl}" srcset="${defaultIconRetinaUrl} 2x" class="leaflet-marker-icon" alt="" style="width: ${iconSize[0]}px; height: ${iconSize[1]}px;">
+        </div>
+        <img src="${defaultShadowUrl}" class="leaflet-marker-shadow" alt="" style="width: 41px; height: 41px; left: 0px; top: 0px;">
+    `;
+
+    return L.divIcon({
+        className: 'custom-leaflet-icon-container',
+        html: htmlContent,
+        iconSize: iconSize,
+        iconAnchor: [iconSize[0] / 2, iconSize[1]],
+        popupAnchor: [1, -34],
+    });
+};
+
+
 function CourtMap({ courts, userLocation, isProximityFilteringActive }: CourtMapProps) {
     const initialCenter: [number, number] = [34.0522, -118.2437];
     const defaultZoom = 10;
@@ -29,7 +52,6 @@ function CourtMap({ courts, userLocation, isProximityFilteringActive }: CourtMap
 
     let mapCenter = initialCenter;
     let mapZoom = defaultZoom;
-
 
     if (userLocation && isProximityFilteringActive && courtsWithCoords.length > 0) {
         const nearestCourt = courtsWithCoords.find(court => court.distanceKm !== undefined);
@@ -50,7 +72,7 @@ function CourtMap({ courts, userLocation, isProximityFilteringActive }: CourtMap
         mapCenter = [avgLat, avgLon];
 
         if (courtsWithCoords.length === 1) {
-            mapZoom = 20;
+            mapZoom = 13;
         } else {
             mapZoom = wideOverviewZoom;
         }
@@ -66,26 +88,33 @@ function CourtMap({ courts, userLocation, isProximityFilteringActive }: CourtMap
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
 
-                    {courtsWithCoords.map(court => (
-                        <Marker key={court.id} position={[court.latitude!, court.longitude!]} >
-                            <Popup>
-                                <strong>{court.name}</strong><br />
-                                {court.available} courts available<br />
-                                <Link to={`/court/${court.id}`}>Details</Link>
-                                <br />
-                                {court.address && (
-                                    <a
-                                        href={`https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(court.address)}`}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        style={{ display: 'inline-block', marginTop: '5px', color: '#1e3a8a', textDecoration: 'none', fontWeight: 'bold' }}
-                                    >
-                                        Get Directions (Google Maps)
-                                    </a>
-                                )}
-                            </Popup>
-                        </Marker>
-                    ))}
+                    {courtsWithCoords.map(court => {
+                        const isUnavailable = court.available === 0;
+                        return (
+                            <Marker
+                                key={court.id}
+                                position={[court.latitude!, court.longitude!]}
+                                icon={createStrikethroughIcon(isUnavailable)}
+                            >
+                                <Popup>
+                                    <strong>{court.name}</strong><br />
+                                    {court.available} courts available<br />
+                                    <Link to={`/court/${court.id}`}>Details</Link>
+                                    <br />
+                                    {court.address && (
+                                        <a
+                                            href={`https://www.google.com/maps/dir/?api=1&destination=${encodeURIComponent(court.address)}`}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            style={{ display: 'inline-block', marginTop: '5px', color: '#1e3a8a', textDecoration: 'none', fontWeight: 'bold' }}
+                                        >
+                                            Get Directions (Google Maps)
+                                        </a>
+                                    )}
+                                </Popup>
+                            </Marker>
+                        );
+                    })}
                 </MapContainer>
             </div>
 
